7d4cc334b9722a51267eed1eda89d4d9a097bdc6
Aderly Joel Vasquez Villalobos <vasquezader@gmail.com>
2017-12-01 20:02:34 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+use Kachkaev\PHPR\RCore;
+use Kachkaev\PHPR\Engine\CommandLineREngine;
+
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', 'index');
+$app->run();
+
+
+function  index (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    
+
+	//$args = array();
+	//$args['pathToErrorFile'] = 'D:/error.txt';
+	//$r = new RCore(new CommandLineREngine('C:\Program Files\R\R-3.4.2\bin\R.exe',$args));
+
+	$r = new RCore(new CommandLineREngine('/usr/bin/R'));
+	
+	$result = $r->run('a = 1
+	b = 2
+	a + b',true);
+	
+	//$response->getBody()->write("Hello, $result");
+    //return $response;
+	return $result[0];
+};
+
+/*
+*TODO:
+*
+*D:\Apache2\htdocs\devops\vendor\bin\phpunit --bootstrap Email.php --testdox tests  
+*D:\Apache2\htdocs\devops\vendor\bin\phpunit --bootstrap Email.php tests/EmailTest
+*
+*/
+?>
